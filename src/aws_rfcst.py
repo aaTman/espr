f6c48754bfe0b019ce2fed81bfac502f1e03ce4f
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2021-05-27 21:16:54 -0400

    fixing the asyncio run

--- src/aws_rfcst.py
+++ src/aws_rfcst.py
@@ -188,4 +188,4 @@ async def download_process_reforecast(
        [await pull_compress(fpath, files, selection_dict, final_path) for files in s3_list_gen]
     
 if __name__ == "__main__":
-    asyncio.run(download_process_reforecast())
+    asyncio.run_coroutine_threadsafe(download_process_reforecast())
