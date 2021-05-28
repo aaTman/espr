29218ef6af0d8cae03d549cd75342b743b29d6f0
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2021-05-27 21:34:14 -0400

    aaaahhhhh

--- src/aws_rfcst.py
+++ src/aws_rfcst.py
@@ -188,4 +188,5 @@ async def download_process_reforecast(
        [await pull_compress(fpath, files, selection_dict, final_path) for files in s3_list_gen]
     
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     asyncio.run(download_process_reforecast())
