3be78b075496ef9478d29c49f4d69747ac37e8ea
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2022-01-26 00:23:44 -0500

    try trustenv

--- espr/gefs_retrieve.py
+++ espr/gefs_retrieve.py
@@ -223,7 +223,7 @@ class GEFSRetrieve:
         self.sprd_fhour_links = [f"{new_link}file={self.ensemble_dict['sprd']}.t{self.hour_value.strip('/')}z.pgrb2s.0p25.f{n:03}" for n in np.arange(0,self.hour_end+1,self.freq)]
 
     async def download_link(self, link):
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(trust_env=True) as session:
                 async with session.get(link, timeout=0) as resp:
                     if resp.status < 400: 
                         content = await resp.read()    
