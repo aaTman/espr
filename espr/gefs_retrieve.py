1bbca1bf522ee07a9712ef13baa3044641dfc7b9
aaTman <tmandelbaum@linevisioninc.com>
2022-01-27 23:14:39 -0500

    oops

--- espr/gefs_retrieve.py
+++ espr/gefs_retrieve.py
@@ -266,15 +266,6 @@ class GEFSRetrieve:
             hour frequency: {self.freq}\n \
             end hour: {self.hour_end}\n \
             directory: {self.download_dir}')   
-        local_filename = url.split('/')[-1]
-    # NOTE the stream=True parameter below
-    
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk: 
-                f.write(chunk)
         for link in links:  
             with requests.get(link, stream=True) as r:
                 r.raise_for_status()
